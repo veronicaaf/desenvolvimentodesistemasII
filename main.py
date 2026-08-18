702caa3a59c2e7fc95938829b31ee9f232fe9a0c
veronicaaf <veronicaafracaro@gmail.com>
2026-08-18 20:32:40 -0300

    padrões de código limpo

---
+++ main.py
@@ -0,0 +1,52 @@
+#x=10
+#y=20
+#z=x+y
+#print(z)
+
+#qualidade no nome de variaveis
+quantidade_de_produtos = 10
+quantidade_de_clientes = 20
+total = quantidade_de_produtos + quantidade_de_clientes
+print (total)
+
+#qualidade no espaçamento do codigo
+nome = "PROFRAFAEL"
+idade = 30
+
+if idade >= 18 :
+    print("Maior de idade")
+
+    x=10
+    y=5
+    z=x*y
+    print(z)
+
+    preco_do_produto=10
+    quantidade=5
+    total = preco_do_produto * quantidade
+    print ("O total de compras foi: " + str (total))
+
+
+#procurar nomes que façam sentido
+nome = "Nome 1"
+senha = "Senha 1"
+
+#padrões  de case sensitive
+precodoproduto=0
+PrecoDoProduto=10
+
+#snake case
+preco_do_produto=10
+
+#camel case
+class Minha classe:
+    pass
+
+def calcula_soma ():
+
+#padroes em expressoes:
+def calcula_soma(valor_saldo, valor_depositado):
+    return valor_saldo + valor_depositado
+    
+valor_do_saldo_final = calcula_soma (500, 500)
+print("O valor final é: " , valor_do_saldo_final)
