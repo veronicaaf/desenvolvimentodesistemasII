3483ea61690280d592a47f80926194316ed80642
veronicaaf <veronicaafracaro@gmail.com>
2026-09-08 23:41:35 +0000

    POO 08/09 - DS II

---
+++ poo.py
@@ -0,0 +1,14 @@
+#Compilada vs Interpretação
+
+
+## Integer numero = new Integer ();
+
+class PessoaAdulta:
+    def _init_(self,nome, idade):
+        self.nome = nome 
+        self.idade= idade 
+
+pessoa = PessoaAdulta("Geraldo, 17")
+
+print (pessoa.nome)
+print (pessoa.idade)
